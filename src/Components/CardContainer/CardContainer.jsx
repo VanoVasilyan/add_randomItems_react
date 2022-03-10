c868e5efab636504781d3fe91ccb9e72a3f2b30f
Vano <vanovasilyan@gmail.com>
2022-03-11 01:34:07 +0400

    https://github.com/VanoVasilyan/Add-randomItems-React.git

--- src/Components/CardContainer/CardContainer.jsx
+++ src/Components/CardContainer/CardContainer.jsx
@@ -1,10 +1,18 @@
 import CardItem from '../CardItem'
 import './CardContainer.css'
 import { GlobalContext } from '../../context';
+import { useEffect } from 'react';
+
 
 
 const CardContainer = () => {
-    const { card, isCardEmpty, mainBackgroundColor } = GlobalContext()
+    const { card, isCardEmpty, mainBackgroundColor, setMainBackgroundColor } = GlobalContext()
+
+    useEffect(() => {
+        if (!card.length) {
+            setMainBackgroundColor('')
+        }
+    }, [card, setMainBackgroundColor])
 
     return <div className='main-card-cont'
         style={{ backgroundColor: isCardEmpty ? 'inherit' : mainBackgroundColor }}
